156617e71d60b5b1123a0f2e07f64d1f7c848714
김유빈 <yubin6894@gmail.com>
2022-10-31 00:26:16 +0900

    TableView를 활용한 메모장 앱 구현하기 - Cell 재사용 문제 해결 / TableView Swipe 기능까지

---
+++ iOS_week4/iOS_week4/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  iOS_week4
+//
+//  Created by 김유빈 on 2022/10/25.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
